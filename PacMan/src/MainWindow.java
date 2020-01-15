b12427de72652a8b0068465b4bd8c83d0bf0e4d4
Felpat0 <b994@live.com>
2020-01-16 00:11:54 +0100

    Added cycle

--- PacMan/src/MainWindow.java
+++ PacMan/src/MainWindow.java
@@ -1,26 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MainWindow extends JFrame{
 	JLabel backgroundLabel;
-	JPanel main;
+	JLabel pg;
 	int x = 0;
 	public MainWindow(){
 		super("Pac-Man");
 		setSize(900, 960);
-		
+
 		Icon background = new ImageIcon(getClass().getResource("background.png"));
 		backgroundLabel = new JLabel(background);
-		main = new JPanel();
-		main.add(backgroundLabel);
-		add(main);
+		pg = new JLabel(background);
+		add(pg);
+		add(backgroundLabel);
+		gameCycle();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setFocusable(true);
         setLocationRelativeTo(null);
         setResizable(false);
 		setVisible(true);
-		gameCycle();
 	}
 	
 	public void gameCycle(){
-		backgroundLabel.setBounds(x+1, x+1, 50, 50);
+		Timer t = new Timer(30, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pg.setBounds(x+5, x+5, 50, 50);
+				x++;
+			}
+		});
+		t.start();
 	}
 }
