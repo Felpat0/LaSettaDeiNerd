b3f6d9db01b89814d80b8a0f689b07fe492605a9
Felpat0 <b994@live.com>
2020-01-15 17:05:30 +0100

    Update MainWindow.java

--- PacMan/src/MainWindow.java
+++ PacMan/src/MainWindow.java
@@ -1,9 +1,17 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class MainWindow extends JFrame{
+	JLabel backgroundLabel;
 	public MainWindow(){
 		super("Pac-Man");
 		setSize(900, 960);
+		
+		Icon background = new ImageIcon(getClass().getResource("background.png"));
+		
+		backgroundLabel = new JLabel(background);
+		add(backgroundLabel);
+		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 		setVisible(true);
